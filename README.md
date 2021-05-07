52a49c9acb3483c459ce938c396ab6e9b594c4bb
JurijTSL <jurij.j.fortuna@gmail.com>
2021-05-07 13:01:29 +0200

    Added support for other currencies

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 A nice Wallpaper Engine powered wallpaper that shows current and past prices of Ethereum cryptocurrency.
 <br/>
 <br/>
-![Normal Mode preview](https://raw.githubusercontent.com/JurijTSL/ethereum-price-background/main/preview-git.jpg)
-
-
-![Dark Mode preview](https://raw.githubusercontent.com/JurijTSL/ethereum-price-background/main/preview-git-dark.jpg)
+![Background preview](https://raw.githubusercontent.com/JurijTSL/ethereum-price-background/main/preview.png)
 
 ## Installing
 Get it on [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2458069432)
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-window.wallpaperPropertyListener = {
-    applyUserProperties: function(properties) {
-        if (properties.style) {
-            if (properties.style.value) {
-                switch (properties.style.value) {
-                    case "normal":
-                        $("#area").css("--bckgColor", "#4e54c8");
-                        break;
-                    case "dark":
-                        $("#area").css("--bckgColor", "#202225");
-                        break;
-                }
-            }
-        }
-    }
-}
+var currency = "dontexist";
 
+window.wallpaperPropertyListener = {
+	applyUserProperties: function(properties) {
+		if (properties.style) {
+			switch (properties.style.value) {
+				case "normal":
+					$("#area").css("--bckgColor", "#4e54c8");
+					break;
+				case "dark":
+					$("#area").css("--bckgColor", "#202225");
+					break;
+			}
+		}
+
+		if (properties.currency) {
+			currency = properties.currency.value;
+			retreivePrice();
+			updateGraph();
+			updateIcon();
+		}
+	}
+}
 
 var chartData = new Array();
 var chartLabels = new Array();
@@ -23,127 +29,167 @@ var ethCanvas = document.getElementById("speedChart");
 Chart.defaults.global.defaultFontFamily = "Varela Round";
 
 var speedData = {
-    labels: chartLabels,
-    datasets: [{
-        label: "Car Speed",
-        data: chartData,
-        lineTension: 0,
-        fill: false,
-        borderColor: '#FFFFFF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#FFFFFF',
-        pointBackgroundColor: '#FFFFFF',
-        pointRadius: 0,
-        pointHoverRadius: 5,
-        pointHitRadius: 5,
-        pointBorderWidth: 2,
-        pointStyle: 'point'
-    }]
+	labels: chartLabels,
+	datasets: [{
+		label: "Car Speed",
+		data: chartData,
+		lineTension: 0,
+		fill: false,
+		borderColor: '#FFFFFF',
+		backgroundColor: 'transparent',
+		pointBorderColor: '#FFFFFF',
+		pointBackgroundColor: '#FFFFFF',
+		pointRadius: 0,
+		pointHoverRadius: 5,
+		pointHitRadius: 5,
+		pointBorderWidth: 2,
+		pointStyle: 'point'
+	}]
 };
 
 var chartOptions = {
-    tooltips: {
-        callbacks: {
-            label: function(tooltipItems, data) {
-                return "$ " + tooltipItems.yLabel.toString();
-            }
-        }
-    },
+	tooltips: {
+		callbacks: {
+			label: function(tooltipItems, data) {
+				return "$ " + tooltipItems.yLabel.toString();
+			}
+		}
+	},
 
-    legend: {
-        display: false
-    },
+	legend: {
+		display: false
+	},
 
-    scales: {
-        xAxes: [{
-            gridLines: {
-                display: true,
-                color: "white",
-                borderDash: [2, 5]
-            },
+	scales: {
+		xAxes: [{
+			gridLines: {
+				display: true,
+				color: "white",
+				borderDash: [2, 5]
+			},
 
-            ticks: {
-                display: true,
-                fontColor: "white",
-                maxTicksLimit: 7,
-                maxRotation: 0,
-                minRotation: 0
-            }
-        }],
+			ticks: {
+				display: true,
+				fontColor: "white",
+				maxTicksLimit: 7,
+				maxRotation: 0,
+				minRotation: 0
+			}
+		}],
 
-        yAxes: [{
-            gridLines: {
-                color: "white",
-                borderDash: [2, 5]
-            },
+		yAxes: [{
+			gridLines: {
+				color: "white",
+				borderDash: [2, 5]
+			},
 
-            ticks: {
-                fontColor: "white"
-            }
-        }]
-    }
+			ticks: {
+				fontColor: "white"
+			}
+		}]
+	}
 };
 
 var lineChart = new Chart(ethCanvas, {
-    type: 'line',
-    data: speedData,
-    options: chartOptions
+	type: 'line',
+	data: speedData,
+	options: chartOptions
 });
+
+function updateIcon() {
+	switch (currency) {
+		case "ethereum":
+			$("#icon").attr("src", "icon/ethereum.svg")
+			break;
+
+		case "bitcoin":
+			$("#icon").attr("src", "icon/bitcoin.svg")
+			break;
+
+		case "dogecoin":
+			$("#icon").attr("src", "icon/dogecoin.svg")
+			break;
+
+		case "litecoin":
+			$("#icon").attr("src", "icon/litecoin.svg")
+			break;
+
+		case "polkadot":
+			$("#icon").attr("src", "icon/polkadot.svg")
+			break;
+
+		case "solana":
+			$("#icon").attr("src", "icon/solana.svg")
+			break;
+	}
+}
 
 let price = 0;
 
 function formateDate(timestamp) {
-    var date = new Date(timestamp);
+	var date = new Date(timestamp);
 
-    var month = date.getMonth() + 1;
-    if (month < 10) {
-        month = "0" + month;
-    }
+	var month = date.getMonth() + 1;
+	if (month < 10) {
+		month = "0" + month;
+	}
 
-    return month + '/' + date.getDate() + '/' + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	return month + '/' + date.getDate() + '/' + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 };
 
 function retreivePrice() {
-    $.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd", function(data) {
-        var updPrice = data.ethereum.usd;
+	$.get("https://api.coingecko.com/api/v3/simple/price?ids=" + currency + "&vs_currencies=usd", function(data) {
+		var obj = $.parseJSON(JSON.stringify(data));
+		var updPrice = obj[currency].usd;
 
-        chartData.push(updPrice);
-        chartLabels.push(formateDate(Date.now()));
-        chartData.splice(0, 1); 
+		chartData.push(updPrice);
+		chartLabels.push(formateDate(Date.now()));
+		chartData.splice(0, 1);
 
-        lineChart.update();
+		lineChart.update();
 
-        $("#eth-price").text("$" + updPrice);
+		$("#current-price").text("$" + updPrice);
 
-        if (updPrice > price) {
-            $("#arrow-sign").removeAttr('class');
-            $("#arrow-sign").addClass("fas").addClass("arrow").addClass("green").addClass("fa-arrow-up");
-        } else if (updPrice == price) {
+		if (updPrice > price) {
+			$("#arrow-sign").removeAttr('class');
+			$("#arrow-sign").addClass("fas").addClass("arrow").addClass("green").addClass("fa-arrow-up");
+		} else if (updPrice == price) {
 
-        } else {
-            $("#arrow-sign").removeAttr('class');
-            $("#arrow-sign").addClass("fas").addClass("arrow").addClass("red").addClass("fa-arrow-down");
-        }
+		} else {
+			$("#arrow-sign").removeAttr('class');
+			$("#arrow-sign").addClass("fas").addClass("arrow").addClass("red").addClass("fa-arrow-down");
+		}
 
-        price = updPrice;
-    });
+		price = updPrice;
+	});
 }
 
-$.get("https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=1", function(data) {
-    var i;
-    for (i = 0; i < data.prices.length; i++) {
-        var priceToAdd = parseFloat(data.prices[i][1]).toFixed(2);
-        var label = data.prices[i][0];
+function updateGraph() {
+	while (chartData.length) {
+		chartData.pop();
+	}
 
-        chartData.push(priceToAdd);
-        chartLabels.push(formateDate(label));
-    }
+	while (chartLabels.length) {
+		chartLabels.pop();
+	}
 
-    lineChart.update();
-});
+	$.get("https://api.coingecko.com/api/v3/coins/" + currency + "/market_chart?vs_currency=usd&days=1", function(data) {
+		var i;
+		for (i = 0; i < data.prices.length; i++) {
+			var priceToAdd = parseFloat(data.prices[i][1]).toFixed(2);
+			var label = data.prices[i][0];
+
+			chartData.push(priceToAdd);
+			chartLabels.push(formateDate(label));
+		}
+
+		lineChart.update();
+	});
+}
 
 setInterval(function() {
-    retreivePrice();
+	retreivePrice();
 }, 300000);
 
 retreivePrice();
+updateGraph();
